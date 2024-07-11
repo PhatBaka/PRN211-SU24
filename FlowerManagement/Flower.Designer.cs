daa60bef6fbcec2f44665e985b474edeb1a0a255
DangQuangTien <tiendqse171443@fpt.edu.vn>
2024-07-11 16:38:04 +0700

    Flower 2

--- FlowerManagement/Flower.Designer.cs
+++ FlowerManagement/Flower.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FlowerManagement
 {
-    partial class Flower
+    partial class FlowerBouquetID
     {
         /// <summary>
         /// Required designer variable.
@@ -28,18 +28,162 @@
         /// </summary>
         private void InitializeComponent()
         {
+            txtFlowerBouquetID = new TextBox();
+            txtFlowerBouquetName = new TextBox();
+            txtDescription = new TextBox();
+            txtUnitPrice = new TextBox();
+            txtMorphology = new TextBox();
+            txtUnitsInStock = new TextBox();
+            txtCategoryID = new TextBox();
+            txtSupplierID = new TextBox();
+            chkFlowerBouquetStatus = new CheckBox();
+            picImage = new PictureBox();
+            btnLoad = new Button();
+            btnsave = new Button();
+            ((System.ComponentModel.ISupportInitialize)picImage).BeginInit();
             SuspendLayout();
             // 
-            // Flower
+            // txtFlowerBouquetID
+            // 
+            txtFlowerBouquetID.Location = new Point(289, 60);
+            txtFlowerBouquetID.Name = "txtFlowerBouquetID";
+            txtFlowerBouquetID.Size = new Size(230, 27);
+            txtFlowerBouquetID.TabIndex = 0;
+            txtFlowerBouquetID.TextChanged += txtFlowerBouquetID_TextChanged;
+            // 
+            // txtFlowerBouquetName
+            // 
+            txtFlowerBouquetName.Location = new Point(289, 93);
+            txtFlowerBouquetName.Name = "txtFlowerBouquetName";
+            txtFlowerBouquetName.Size = new Size(230, 27);
+            txtFlowerBouquetName.TabIndex = 1;
+            txtFlowerBouquetName.TextChanged += txtFlowerBouquetName_TextChanged;
+            // 
+            // txtDescription
+            // 
+            txtDescription.Location = new Point(289, 126);
+            txtDescription.Name = "txtDescription";
+            txtDescription.Size = new Size(230, 27);
+            txtDescription.TabIndex = 2;
+            txtDescription.TextChanged += txtDescription_TextChanged;
+            // 
+            // txtUnitPrice
+            // 
+            txtUnitPrice.Location = new Point(289, 159);
+            txtUnitPrice.Name = "txtUnitPrice";
+            txtUnitPrice.Size = new Size(230, 27);
+            txtUnitPrice.TabIndex = 3;
+            txtUnitPrice.TextChanged += txtUnitPrice_TextChanged;
+            // 
+            // txtMorphology
+            // 
+            txtMorphology.Location = new Point(525, 93);
+            txtMorphology.Name = "txtMorphology";
+            txtMorphology.Size = new Size(230, 27);
+            txtMorphology.TabIndex = 4;
+            txtMorphology.TextChanged += txtMorphology_TextChanged;
+            // 
+            // txtUnitsInStock
+            // 
+            txtUnitsInStock.Location = new Point(525, 60);
+            txtUnitsInStock.Name = "txtUnitsInStock";
+            txtUnitsInStock.Size = new Size(230, 27);
+            txtUnitsInStock.TabIndex = 5;
+            txtUnitsInStock.TextChanged += txtUnitsInStock_TextChanged;
+            // 
+            // txtCategoryID
+            // 
+            txtCategoryID.Location = new Point(525, 126);
+            txtCategoryID.Name = "txtCategoryID";
+            txtCategoryID.Size = new Size(230, 27);
+            txtCategoryID.TabIndex = 6;
+            txtCategoryID.TextChanged += txtCategoryID_TextChanged;
+            // 
+            // txtSupplierID
+            // 
+            txtSupplierID.Location = new Point(525, 159);
+            txtSupplierID.Name = "txtSupplierID";
+            txtSupplierID.Size = new Size(230, 27);
+            txtSupplierID.TabIndex = 7;
+            txtSupplierID.TextChanged += txtSupplierID_TextChanged;
+            // 
+            // chkFlowerBouquetStatus
+            // 
+            chkFlowerBouquetStatus.AutoSize = true;
+            chkFlowerBouquetStatus.Location = new Point(326, 195);
+            chkFlowerBouquetStatus.Name = "chkFlowerBouquetStatus";
+            chkFlowerBouquetStatus.Size = new Size(171, 24);
+            chkFlowerBouquetStatus.TabIndex = 8;
+            chkFlowerBouquetStatus.Text = "FlowerBouquetStatus";
+            chkFlowerBouquetStatus.UseVisualStyleBackColor = true;
+            chkFlowerBouquetStatus.CheckedChanged += chkFlowerBouquetStatus_CheckedChanged;
+            // 
+            // picImage
+            // 
+            picImage.Location = new Point(81, 46);
+            picImage.Name = "picImage";
+            picImage.Size = new Size(202, 140);
+            picImage.TabIndex = 9;
+            picImage.TabStop = false;
+            picImage.Click += picImage_Click;
+            // 
+            // btnLoad
+            // 
+            btnLoad.Location = new Point(534, 192);
+            btnLoad.Name = "btnLoad";
+            btnLoad.Size = new Size(94, 29);
+            btnLoad.TabIndex = 10;
+            btnLoad.Text = "Load";
+            btnLoad.UseVisualStyleBackColor = true;
+            btnLoad.Click += btnLoad_Click;
+            // 
+            // btnsave
+            // 
+            btnsave.Location = new Point(649, 194);
+            btnsave.Name = "btnsave";
+            btnsave.Size = new Size(82, 25);
+            btnsave.TabIndex = 11;
+            btnsave.Text = "Save";
+            btnsave.UseVisualStyleBackColor = true;
+            btnsave.Click += btnsave_Click;
+            // 
+            // FlowerBouquetID
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Name = "Flower";
+            Controls.Add(btnsave);
+            Controls.Add(btnLoad);
+            Controls.Add(picImage);
+            Controls.Add(chkFlowerBouquetStatus);
+            Controls.Add(txtSupplierID);
+            Controls.Add(txtCategoryID);
+            Controls.Add(txtUnitsInStock);
+            Controls.Add(txtMorphology);
+            Controls.Add(txtUnitPrice);
+            Controls.Add(txtDescription);
+            Controls.Add(txtFlowerBouquetName);
+            Controls.Add(txtFlowerBouquetID);
+            Name = "FlowerBouquetID";
             Text = "Flower";
+            ((System.ComponentModel.ISupportInitialize)picImage).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtFlowerBouquetID;
+        private TextBox txtFlowerBouquetName;
+        private TextBox txtDescription;
+        private TextBox txtUnitPrice;
+        private TextBox txtMorphology;
+        private TextBox txtUnitsInStock;
+        private TextBox txtCategoryID;
+        private TextBox txtSupplierID;
+        private CheckBox chkFlowerBouquetStatus;
+        private PictureBox picImage;
+        private Button btnLoad;
+        private Button btnsave;
     }
 }
--- FlowerManagement/Flower.cs
+++ FlowerManagement/Flower.cs
@@ -3,18 +3,189 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BusinessObjects;
+using DataAccessObjects;
 
 namespace FlowerManagement
 {
-    public partial class Flower : Form
+    public partial class FlowerBouquetID : Form
     {
-        public Flower()
+        private Flower currentFlower;
+
+        public FlowerBouquetID()
         {
             InitializeComponent();
+        }
+
+        private void chkFlowerBouquetStatus_CheckedChanged(object sender, EventArgs e)
+        {
+            if (currentFlower != null)
+            {
+                currentFlower.FlowerBouquetStatus = chkFlowerBouquetStatus.Checked;
+            }
+        }
+
+        private void picImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string imagePath = openFileDialog.FileName;
+                picImage.Image = Image.FromFile(imagePath);
+                currentFlower.Image = File.ReadAllBytes(imagePath);
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            int flowerBouquetID;
+            if (int.TryParse(txtFlowerBouquetID.Text, out flowerBouquetID))
+            {
+
+                currentFlower = FlowerDataAccess.GetFlowerByID(flowerBouquetID);
+
+                if (currentFlower != null)
+                {
+                    txtFlowerBouquetName.Text = currentFlower.FlowerBouquetName;
+                    txtDescription.Text = currentFlower.Description;
+                    txtUnitPrice.Text = currentFlower.UnitPrice.ToString();
+                    txtUnitsInStock.Text = currentFlower.UnitsInStock.ToString();
+                    chkFlowerBouquetStatus.Checked = currentFlower.FlowerBouquetStatus;
+                    txtMorphology.Text = currentFlower.Morphology;
+                    txtCategoryID.Text = currentFlower.CategoryID.ToString();
+                    txtSupplierID.Text = currentFlower.SupplierID.ToString();
+
+                    if (currentFlower.Image != null)
+                    {
+                        using (MemoryStream ms = new MemoryStream(currentFlower.Image))
+                        {
+                            picImage.Image = Image.FromStream(ms);
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Flower not found.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid Flower Bouquet ID.");
+            }
+        }
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            if (currentFlower != null)
+            {
+
+                currentFlower.FlowerBouquetName = txtFlowerBouquetName.Text;
+                currentFlower.Description = txtDescription.Text;
+                currentFlower.UnitPrice = decimal.Parse(txtUnitPrice.Text);
+                currentFlower.UnitsInStock = int.Parse(txtUnitsInStock.Text);
+                currentFlower.FlowerBouquetStatus = chkFlowerBouquetStatus.Checked;
+                currentFlower.Morphology = txtMorphology.Text;
+                currentFlower.CategoryID = int.Parse(txtCategoryID.Text);
+                currentFlower.SupplierID = int.Parse(txtSupplierID.Text);
+
+                FlowerDataAccess.SaveFlower(currentFlower);
+                MessageBox.Show("Flower data saved successfully.");
+            }
+            else
+            {
+                MessageBox.Show("No Flower data to save.");
+            }
+
+        }
+
+        private void txtFlowerBouquetID_TextChanged(object sender, EventArgs e)
+        {
+            if (currentFlower != null)
+            {
+                int flowerBouquetID;
+                if (int.TryParse(txtFlowerBouquetID.Text, out flowerBouquetID))
+                {
+                    currentFlower.FlowerBouquetID = flowerBouquetID;
+                }
+            }
+        }
+
+        private void txtFlowerBouquetName_TextChanged(object sender, EventArgs e)
+        {
+            if (currentFlower != null)
+            {
+                currentFlower.FlowerBouquetName = txtFlowerBouquetName.Text;
+            }
+        }
+
+        private void txtDescription_TextChanged(object sender, EventArgs e)
+        {
+            if (currentFlower != null)
+            {
+                currentFlower.Description = txtDescription.Text;
+            }
+        }
+
+        private void txtUnitPrice_TextChanged(object sender, EventArgs e)
+        {
+            if (currentFlower != null)
+            {
+                decimal unitPrice;
+                if (decimal.TryParse(txtUnitPrice.Text, out unitPrice))
+                {
+                    currentFlower.UnitPrice = unitPrice;
+                }
+            }
+        }
+
+        private void txtUnitsInStock_TextChanged(object sender, EventArgs e)
+        {
+            if (currentFlower != null)
+            {
+                int unitsInStock;
+                if (int.TryParse(txtUnitsInStock.Text, out unitsInStock))
+                {
+                    currentFlower.UnitsInStock = unitsInStock;
+                }
+            }
+        }
+
+        private void txtMorphology_TextChanged(object sender, EventArgs e)
+        {
+            if (currentFlower != null)
+            {
+                currentFlower.Morphology = txtMorphology.Text;
+            }
+        }
+
+        private void txtCategoryID_TextChanged(object sender, EventArgs e)
+        {
+            if (currentFlower != null)
+            {
+                int categoryID;
+                if (int.TryParse(txtCategoryID.Text, out categoryID))
+                {
+                    currentFlower.CategoryID = categoryID;
+                }
+            }
+        }
+
+        private void txtSupplierID_TextChanged(object sender, EventArgs e)
+        {
+            if (currentFlower != null)
+            {
+                int supplierID;
+                if (int.TryParse(txtSupplierID.Text, out supplierID))
+                {
+                    currentFlower.SupplierID = supplierID;
+                }
+            }
         }
 
 

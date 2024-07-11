cceebace8eb0340ba5e278797139df3847e0ba01
DangQuangTien <tiendqse171443@fpt.edu.vn>
2024-07-11 20:06:50 +0700

    Supplier

---
+++ FlowerManagement/Supplier.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace FlowerManagement
+{
+    partial class Supplier
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtSupplierID = new TextBox();
+            txtSupplierName = new TextBox();
+            txtSupplierAddress = new TextBox();
+            txtTelephone = new TextBox();
+            btnLoadSupplier = new Button();
+            btnSaveSupplier = new Button();
+            SuspendLayout();
+            // 
+            // txtSupplierID
+            // 
+            txtSupplierID.Location = new Point(273, 122);
+            txtSupplierID.Name = "txtSupplierID";
+            txtSupplierID.Size = new Size(217, 27);
+            txtSupplierID.TabIndex = 0;
+            txtSupplierID.TextChanged += txtSupplierID_TextChanged;
+            // 
+            // txtSupplierName
+            // 
+            txtSupplierName.Location = new Point(273, 155);
+            txtSupplierName.Name = "txtSupplierName";
+            txtSupplierName.Size = new Size(217, 27);
+            txtSupplierName.TabIndex = 1;
+            txtSupplierName.TextChanged += txtSupplierName_TextChanged;
+            // 
+            // txtSupplierAddress
+            // 
+            txtSupplierAddress.Location = new Point(273, 188);
+            txtSupplierAddress.Name = "txtSupplierAddress";
+            txtSupplierAddress.Size = new Size(217, 27);
+            txtSupplierAddress.TabIndex = 2;
+            txtSupplierAddress.TextChanged += txtSupplierAddress_TextChanged;
+            // 
+            // txtTelephone
+            // 
+            txtTelephone.Location = new Point(273, 221);
+            txtTelephone.Name = "txtTelephone";
+            txtTelephone.Size = new Size(217, 27);
+            txtTelephone.TabIndex = 3;
+            txtTelephone.TextChanged += txtTelephone_TextChanged;
+            // 
+            // btnLoadSupplier
+            // 
+            btnLoadSupplier.Location = new Point(254, 254);
+            btnLoadSupplier.Name = "btnLoadSupplier";
+            btnLoadSupplier.Size = new Size(127, 29);
+            btnLoadSupplier.TabIndex = 4;
+            btnLoadSupplier.Text = "Load Supplier";
+            btnLoadSupplier.UseVisualStyleBackColor = true;
+            btnLoadSupplier.Click += btnLoadSupplier_Click;
+            // 
+            // btnSaveSupplier
+            // 
+            btnSaveSupplier.Location = new Point(400, 254);
+            btnSaveSupplier.Name = "btnSaveSupplier";
+            btnSaveSupplier.Size = new Size(129, 29);
+            btnSaveSupplier.TabIndex = 5;
+            btnSaveSupplier.Text = "Save Supplier";
+            btnSaveSupplier.UseVisualStyleBackColor = true;
+            btnSaveSupplier.Click += btnSaveSupplier_Click;
+            // 
+            // Supplier
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnSaveSupplier);
+            Controls.Add(btnLoadSupplier);
+            Controls.Add(txtTelephone);
+            Controls.Add(txtSupplierAddress);
+            Controls.Add(txtSupplierName);
+            Controls.Add(txtSupplierID);
+            Name = "Supplier";
+            Text = "Supplier";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtSupplierID;
+        private TextBox txtSupplierName;
+        private TextBox txtSupplierAddress;
+        private TextBox txtTelephone;
+        private Button btnLoadSupplier;
+        private Button btnSaveSupplier;
+    }
+}
---
+++ FlowerManagement/Supplier.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BusinessObjects;
+using DataAccessObjects;
+
+namespace FlowerManagement
+{
+    public partial class Supplier : Form
+    {
+        private BusinessObjects.Supplier currentSupplier;
+
+        public Supplier()
+        {
+            InitializeComponent();
+        }
+
+        private void txtSupplierID_TextChanged(object sender, EventArgs e)
+        {
+            if (currentSupplier != null)
+            {
+                int supplierID;
+                if (int.TryParse(txtSupplierID.Text, out supplierID))
+                {
+                    currentSupplier.SupplierID = supplierID;
+                }
+            }
+        }
+
+        private void txtSupplierName_TextChanged(object sender, EventArgs e)
+        {
+            if (currentSupplier != null)
+            {
+                currentSupplier.SupplierName = txtSupplierName.Text;
+            }
+        }
+
+        private void txtSupplierAddress_TextChanged(object sender, EventArgs e)
+        {
+            if (currentSupplier != null)
+            {
+                currentSupplier.SupplierAddress = txtSupplierAddress.Text;
+            }
+        }
+
+        private void txtTelephone_TextChanged(object sender, EventArgs e)
+        {
+            if (currentSupplier != null)
+            {
+                currentSupplier.Telephone = txtTelephone.Text;
+            }
+        }
+
+        private void btnLoadSupplier_Click(object sender, EventArgs e)
+        {
+            int supplierID;
+            if (int.TryParse(txtSupplierID.Text, out supplierID))
+            {
+
+                currentSupplier = SupplierDataAccess.GetSupplierByID(supplierID);
+
+                if (currentSupplier != null)
+                {
+
+                    txtSupplierName.Text = currentSupplier.SupplierName;
+                    txtSupplierAddress.Text = currentSupplier.SupplierAddress;
+                    txtTelephone.Text = currentSupplier.Telephone;
+                }
+                else
+                {
+                    MessageBox.Show("Supplier not found.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid Supplier ID.");
+            }
+        }
+
+        private void btnSaveSupplier_Click(object sender, EventArgs e)
+        {
+            if (currentSupplier != null)
+            {
+                currentSupplier.SupplierName = txtSupplierName.Text;
+                currentSupplier.SupplierAddress = txtSupplierAddress.Text;
+                currentSupplier.Telephone = txtTelephone.Text;
+
+                SupplierDataAccess.SaveSupplier(currentSupplier);
+                MessageBox.Show("Supplier data saved successfully.");
+            }
+            else
+            {
+                MessageBox.Show("No Supplier data to save.");
+            }
+
+        }
+    }
+}

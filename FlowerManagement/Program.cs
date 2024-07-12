6a58649639fb0477bdad9bf3b33e6af9825dfe31
PhatBaka <nguyenquyphat2711@gmail.com>
2024-07-12 21:02:43 +0700

    login

--- FlowerManagement/Program.cs
+++ FlowerManagement/Program.cs
@@ -16,7 +16,7 @@ namespace FlowerManagement
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new frmOrder());
+            Application.Run(new frmLogin());
         }
     }
 }
